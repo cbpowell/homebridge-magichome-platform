59e161a3b06927a0f3b725376f9f617c8b145e2d
Charles Powell <cbpowell@gmail.com>
2020-04-12 12:44:03 -0700

    Add extra byte for WW LEDs to flux_led.py

--- src/flux_led.py
+++ src/flux_led.py
@@ -568,6 +568,7 @@ class WifiLedBulb():
 		msg.append(0x00)
 		msg.append(0x00)
 		msg.append(utils.percentToByte(level))
+        msg.append(0x00)
 		msg.append(0x0f)
 		msg.append(0x0f)
 		self.__write(msg)
